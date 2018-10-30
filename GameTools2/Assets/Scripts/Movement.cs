76dd2765207789c90519f2852660d419f4ab6e0f
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2018-10-30 18:50:27 +0000

    Added comments to the movement script

--- GameTools2/Assets/Scripts/Movement.cs
+++ GameTools2/Assets/Scripts/Movement.cs
@@ -4,43 +4,39 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    Animator c_anim;
+    Animator c_anim;//Declarign my variable for the animator.
     
-    public Transform BulletSpawn;
-    public GameObject Bullet;
-    public Transform Enemy;
-    public AudioSource FireEffect;
-    public Camera maincam;
-    public Vector3 campos1,campos2;
+    public Transform BulletSpawn;//The transform position where the bullet will spawn from the gun.
+    public GameObject Bullet;//This is used to store the Bullet prefab.
+    public Transform Enemy;//this si for the location of the enemy object.
+    public AudioSource FireEffect;//Sound Effect for the gun shooting.
+    public Camera maincam;//This is used to store the camera so it can be moved in the level.
+    public Vector3 campos1,campos2;//These positions are for where the camera will move to.
     
-    private float BulletSpeed = 2000;
-    public float health = 30f;
-
-    
-
-    Collider BulletCol;
+    private float BulletSpeed = 2000;//This float affects the speed of the bullet.
+    public float health = 30f;//This si the health value for the player and is affected by the zombie script.
 
     void Start ()
     {
-        c_anim = GetComponent<Animator>();
+        c_anim = GetComponent<Animator>();//Assigning the animator component on the game object to the animator varible.
     }
 	
 	
 	void Update ()
     {
-        float move = Input.GetAxis("Vertical");
-        c_anim.SetFloat("forward", move);
-        float strafe = Input.GetAxis("Horizontal");
-        c_anim.SetFloat("strafe", strafe);
-        bool crouch = Input.GetKey(KeyCode.C);
-        c_anim.SetBool("crouched", crouch);
-        bool shoot = Input.GetMouseButtonDown(0);
-        c_anim.SetBool("shoot", shoot);
+        float move = Input.GetAxis("Vertical");//This is so that the W and S keys move the character and play its respected animations.
+        c_anim.SetFloat("forward", move);//This affects the float parameter named forward in the animator controller.
+        float strafe = Input.GetAxis("Horizontal");//This is so that the A and D keys turn the character and play its respected animations.
+        c_anim.SetFloat("strafe", strafe);//This affects the float parameter named strafe in the animator controller.
+        bool crouch = Input.GetKey(KeyCode.C);//This is so that the C key allows the player to crouch and play its respected animation.
+        c_anim.SetBool("crouched", crouch);//This affects the bool parameter named crouched in the animator controller.
+        bool shoot = Input.GetMouseButtonDown(0);//When the player presses left click the bool shoot is set to true.
+        c_anim.SetBool("shoot", shoot);//This affects the bool parameter named shoot in the animator controller.
         
 
 
 
-        if (health <= 0)
+        if (health <= 0)//If the palyer takes enough damage to reduce their health to 0 it plays the die animation adn destroys the gameobject.
         {
             bool die = true;
             c_anim.SetBool("die", die);
@@ -49,24 +45,18 @@ public class Movement : MonoBehaviour {
 
     }
 
-    public void ActiveShoot()
+    public void ActiveShoot()//This function is to shoot a bullet from the gun and is called during the shoot animation when the character pulls the trigger.
     {
-        
-        GameObject Shoot;
-        Rigidbody BulletPhys;
-        //Destroy(Shoot, 3f);
-        Shoot = Instantiate(Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation) as GameObject;
-        FireEffect.Play();
-
-
-        BulletPhys = Shoot.GetComponent<Rigidbody>();
-        BulletPhys.AddForce(transform.forward * BulletSpeed);
-        Destroy(Shoot, 1f);
-
-       
+        GameObject Shoot;//This line is declaring a gameobject to be used to assign the spawning in of the bullet and assigning a rigidbody to the bullet.
+        Rigidbody BulletPhys;//This line is declaring a rigidbody to apply physics to the bullet.
+        Shoot = Instantiate(Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation) as GameObject;//Assigning the spawning of the bullet to the shoot game object.
+        FireEffect.Play();//This is playing the Gun Shot sound effect.
+        BulletPhys = Shoot.GetComponent<Rigidbody>();//This line is assinging the rigidbody component to the variable BulletPhys.
+        BulletPhys.AddForce(transform.forward * BulletSpeed);//This is shooting the bullet forward at the predefined speed.
+        Destroy(Shoot, 1f);//After 1 second the bullet is destroyed.
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//If the player enters a trigger with either the Cam1 or Cam2 tag it will change to position of the camera.
     {
         if(other.tag == "Cam1")
         {
