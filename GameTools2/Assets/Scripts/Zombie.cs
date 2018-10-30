fcc8bcc0a3f2cca4e12dad48f3178a0e5f426a0b
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2018-10-30 19:08:16 +0000

    Added comments to the zombie script

--- GameTools2/Assets/Scripts/Zombie.cs
+++ GameTools2/Assets/Scripts/Zombie.cs
@@ -4,92 +4,68 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour {
 
-    public Transform player;
-    public float speed;
-    Animator z_anim;
-    public float health = 10f;
-    public float damage = 10f;
-    public GameObject Player;
-    public AudioSource Bite,Death;
+    Animator z_anim;//Declaring a variable for the animator.
     
+    public float speed;//Declaring float to be used for the enemies speed.
+    public float health = 10f;//Setting the Health value for the enemy.
+    public float damage = 10f;//Setting how much damage the enemy can deal.
 
-
-
+    public Transform player;//This is taking in a public transform of the player so that the enemy know where the player is.
+    public GameObject Player;//This is taking in the player object so taht it can access its components such as health and its animator
+    public AudioSource Bite,Death;//This is adding in a bite and death sound effect to the zombie.
+    
     void Start ()
     {
-        z_anim = GetComponent<Animator>();
-        
+        z_anim = GetComponent<Animator>();//Assigning the animator variable to the objects animator component.
 	}
 	
 	
 	void Update ()
     {
-        //TakeDamage();
-        Debug.Log(health);
-        float step = speed * Time.deltaTime;
-        float dist = Vector3.Distance(player.position, transform.position);
+        Debug.Log(health);//This is for checking the Health of teh enemy in the console when testing.
 
-        if(dist < 10 && dist >2)
+        float step = speed * Time.deltaTime;//This is so the speed is affected by the seconds in game not the frames.
+        float dist = Vector3.Distance(player.position, transform.position);//Here I'm assigning a float variable to be asigned the value of the distance between the players position and the obejct this zmobie script is attached to,
+
+        if(dist < 10 && dist >2)//If the player gets close enough to the enemy this if statement wll trigger.
         {
-            transform.LookAt(player);
-            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+            transform.LookAt(player);//The zombie will look towards the player.
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);//This is moving the zombie towards the position of the player.
             bool walk = true;
-            z_anim.SetBool("PlayerClose", walk);
+            z_anim.SetBool("PlayerClose", walk);//This is telling the animator to start the zombies wlak animation.
 
-            if(dist<3)
+            if (dist<3)//Once the zombie gets close to the player it will run this if statement which plays an attack animation and stops the zombie from moving.
             {
                 walk = false;
                 z_anim.SetBool("PlayerClose", walk);
                 bool attack = true;
                 z_anim.SetBool("Attack", attack);
-
-                
-
-            }
-           
+            }   
         }
-        else
+        else//If the player is not close to the zombie he stands idle.
         {
            bool walk = false;
            z_anim.SetBool("PlayerClose", walk);
         }
 
-        if (health <= 0)
+        if (health <= 0)//If the player shoots the zombie.
         {
-            Death.Play();
+            Death.Play();//Play the death sound.
             bool walk = false;
-            z_anim.SetBool("PlayerClose", walk);
+            z_anim.SetBool("PlayerClose", walk);//Stop any wlaking animation.
             bool attack = false;
-            z_anim.SetBool("Attack", attack);
+            z_anim.SetBool("Attack", attack);//Stop any attack animation.
             bool die = true;
-            z_anim.SetBool("Die", die);
-            Destroy(gameObject, 3f);
+            z_anim.SetBool("Die", die);//Play the death animation.
+            Destroy(gameObject, 3f);//After 3 seconds destory the gameobject the zombie script is attached to.
         }
     }
 
-    /*public void TakeDamage()
+    public void AttackHit()//This is a event made during the zombie attack animation when it hits the player.
     {
-        if(health<=0)
-        {
-            
-            bool walk = false;
-            z_anim.SetBool("PlayerClose", walk);
-            bool attack = false;
-            z_anim.SetBool("Attack", attack);
-            bool die = true;
-            
-            z_anim.SetBool("Die", die);
-        }
-
-    }*/
-    public void AttackHit()
-    {
-        Bite.Play();
-        GameObject.Find("player").GetComponent<Movement>().health -= damage;
-        Animator c_anim = Player.GetComponent<Animator>();
-        c_anim.SetTrigger("Damage");
-        
+        Bite.Play();//PLays the attack sound effect.
+        GameObject.Find("player").GetComponent<Movement>().health -= damage;//This line finds the player object and the movement script attached to it and deals damage to the health value in movement.
+        Animator c_anim = Player.GetComponent<Animator>();//This is assigning the animator of the player object to a variable.
+        c_anim.SetTrigger("Damage");//This is telling the players animation controller to play the damage animation when he is it.
     }
-
-
 }
