c341bd3c822c9313c522c58c7b57d294c55169ad
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2018-12-15 20:11:57 +0000

    NavMesh following waypoints

--- GameTools2/Assets/Scripts/Zombie.cs
+++ GameTools2/Assets/Scripts/Zombie.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour {
 
+    private enum NPCState { CHASE, PATROL};
+    private NPCState z_NPCState;
+    private int z_CurrentWaypoint;
     Animator z_anim;//Declaring a variable for the animator.
     
     public float speed;//Declaring float to be used for the enemies speed.
@@ -15,20 +19,28 @@ public class Zombie : MonoBehaviour {
     public GameObject Blood;//used to store the blood particle effect.
     public AudioSource Bite,Death,Moan;//This is adding in a bite and death sound effect to the zombie.
     public Transform BloodEffect;//Used to spawn in blood effect.
+    public Transform[] z_Waypoints;
+
+    private NavMeshAgent z_navMeshAgent;
+    
     
     void Start ()
     {
+        z_NPCState = NPCState.PATROL;
+        z_navMeshAgent = GetComponent<NavMeshAgent>();
         z_anim = GetComponent<Animator>();//Assigning the animator variable to the objects animator component. 
+        z_CurrentWaypoint = 0;
+        AnimationFloat();
     }
 	
 	
 	void Update ()
     {
-        Debug.Log(health);//This is for checking the Health of teh enemy in the console when testing.
+       // Debug.Log(health);//This is for checking the Health of the enemy in the console when testing.
         float step = speed * Time.deltaTime;//This is so the speed is affected by the seconds in game not the frames.
         float dist = Vector3.Distance(player.position, transform.position);//Here I'm assigning a float variable to be asigned the value of the distance between the players position and the obejct this zmobie script is attached to,
        
-        if(dist < 60 && dist >2)//If the player gets close enough to the enemy this if statement wll trigger.
+       /* if(dist < 60 && dist >2)//If the player gets close enough to the enemy this if statement wll trigger.
         {
             transform.LookAt(player);//The zombie will look towards the player.
             transform.position = Vector3.MoveTowards(transform.position, player.position, step);//This is moving the zombie towards the position of the player.
@@ -47,6 +59,18 @@ public class Zombie : MonoBehaviour {
         {
            bool walk = false;
            z_anim.SetBool("PlayerClose", walk);
+        }*/
+
+        switch(z_NPCState)
+        {
+            case NPCState.CHASE:
+                Chase();
+                break;
+            case NPCState.PATROL:
+                Patrol();
+                break;
+            default:
+                break;
         }
 
         if (health <= 0)//If the player shoots the zombie.
@@ -73,5 +97,36 @@ public class Zombie : MonoBehaviour {
     public void DeathSound()
     {
         Death.Play();//Play the death sound.
+    }
+
+    void Chase()
+    {
+        z_navMeshAgent.SetDestination(Player.transform.position);
+    }
+
+    void Patrol()
+    {
+        UpdateWaypoints();
+        z_navMeshAgent.SetDestination(z_Waypoints[z_CurrentWaypoint].position);
+    }
+
+    void UpdateWaypoints()
+    {
+        if(Vector3.Distance(z_Waypoints[z_CurrentWaypoint].position,this.transform.position) < 3)
+        {
+            z_CurrentWaypoint = (z_CurrentWaypoint + 1) % z_Waypoints.Length;
+        }
+    }
+
+    void AnimationFloat()
+    {
+        if(z_NPCState == NPCState.CHASE)
+        {
+            z_anim.SetFloat("forward", 2);
+        }
+        else
+        {
+            z_anim.SetFloat("forward", 1);
+        }
     }
 }
